62c993421c8725d3b061011bf3cb0e22a9dffe63
Andrew Jones <andrew.jones@puppet.com>
2023-04-24 17:39:15 +0100

    made the script nicerer

--- get_my_ip.sh
+++ get_my_ip.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 ext_ip=`curl -s https://freedns.afraid.org/dynamic/check.php | grep REMOTE_ADDR |  awk '{print $3}'`
-cidr=$ext_ip"/32"
-echo "{ \"my_ip\" : \""$cidr"\" }"
+echo "{ \"my_ip\" : \""$ext_ip"/32\" }"
